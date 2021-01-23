7a2709986387cc57e0020cbe403ea7c0a35af21b
sullie-dev <eoghan@sullie.dev>
2021-01-23 11:04:05 +0000

    Enhancment
    Added unstyled slider to dymically adjust colors for a single pallete

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <Pallete {...seedColors[4]}/>
+      <Pallete pallete={generatePallete(seedColors[4])}/>
     </div>
   );
 }
--- src/Pallete.js
+++ src/Pallete.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import './Pallete.css'
+import "./Pallete.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 export default class Pallete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-    // console.log(this.props.colors.map((c) => c.name));
-    const colorBoxes = this.props.colors.map((c)=> (
-        <ColorBox background={c.color} name={c.name} key={c.name} />
+    const { colors } = this.props.pallete;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} key={color.name} />
     ));
 
     return (
       <div className="Pallete">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
         {/* TODO(1): NavBar goes here */}
         <div className="Pallete-colors">
           {/* TODO(2): Add in color boxes for pallete */}
