9c2becbd11b3dd9413790e4d33407a1b72416c20
sullie-dev <eoghan@sullie.dev>
2021-01-23 10:45:24 +0000

    Enhancment
    Added color helper to geenrate color values based on pallete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Pallete from './Pallete';
 import seedColors from './seedColours'
+import {generatePallete} from "./colorHelpers"
+
 
 function App() {
+  console.log(generatePallete(seedColors[4]))
+
   return (
     <div className="App">
       <Pallete {...seedColors[4]}/>
