1eacee70abdacbc8f4ea05df90e97fdee61e258e
sullie-dev <eoghan@sullie.dev>
2021-01-23 17:28:48 +0000

    Enhancment
    Added Navbar and styling for navbar

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import Navbar from "./Navbar";
 import "./Pallete.css";
-
 
 export default class Pallete extends Component {
   constructor(props) {
@@ -24,16 +22,8 @@ export default class Pallete extends Component {
 
     return (
       <div className="Pallete">
-        <div className="slider">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
-        </div>
         {/* TODO(1): NavBar goes here */}
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Pallete-colors">
           {/* TODO(2): Add in color boxes for pallete */}
           {colorBoxes}
