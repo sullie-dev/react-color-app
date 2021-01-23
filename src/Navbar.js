1ee36fc435a9d8d89e42c9652a9e04484b1bdd97
sullie-dev <eoghan@sullie.dev>
2021-01-23 18:00:31 +0000

    Enhancment
    Added in select and material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { Select, MenuItem } from "@material-ui/core";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChnage = this.handleChnage.bind(this)
+  }
+
+  handleChnage(e){
+      this.setState({format: e.target.value});
+      this.props.handleChange(e.target.value)
+  }
   render() {
+    const { format } = this.state;
     const { level, changeLevel } = this.props;
     return (
       <header className="Navbar">
@@ -12,7 +24,7 @@ export default class Navbar extends Component {
           <a href="#">React Color Picker</a>
         </div>
         <div className="slider-container">
-            <span>Level: {level}</span>
+          <span>Level: {level}</span>
           <div className="slider">
             <Slider
               defaultValue={level}
@@ -22,6 +34,13 @@ export default class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={this.state.format} onChange={this.handleChnage}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
