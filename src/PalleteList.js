a0806c898ed3eb1f27e1165809d0771eab0f3433
sullie-dev <eoghan@sullie.dev>
2021-01-24 15:09:36 +0000

    Enhancments
    Started mini palette styling

--- src/PalleteList.js
+++ src/PalleteList.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 export default class PalleteList extends Component {
   render() {
     const { palletes } = this.props;
     return (
       <div>
+          <MiniPalette/>
         <h1>Hello tehre</h1>
         {palletes.map((pallete) => (
           <p>
-            <Link to={`/pallete/${pallete.id}`}>{pallete.paletteName}</Link>
+              <MiniPalette {...pallete} />
+            {/* <Link to={`/pallete/${pallete.id}`}>{pallete.paletteName}</Link> */}
           </p>
         ))}
       </div>
