b4b7ee5052d1196fc316d6bce04d135a6325b7fb
sullie-dev <eoghan@sullie.dev>
2021-01-24 14:24:39 +0000

    Enhancment
    Added in correct routing to show pallete based off id

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import Pallete from "./Pallete";
 import { Route, Switch } from "react-router-dom";
 import seedColors from "./seedColours";
 import { generatePallete } from "./colorHelpers";
+import { Component } from "react";
 
-function App() {
-  return (
-    <Switch>
-      <Route exatch path="/" />
-      <Route exact path="pallete/:id" />
-    </Switch>
+class App extends Component {
+  findPallete(id) {
+    return seedColors.find(function (pallete) {
+      return pallete.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Hello there</h1>} />
+        <Route
+          exact
+          path="/:id"
+          render={(routerProps) => (
+            <Pallete
+              pallete={generatePallete(
+                this.findPallete(routerProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
 
-    // <div className="App">
-    //   <Pallete pallete={generatePallete(seedColors[4])}/>
-    // </div>
-  );
+      // <div className="App">
+      //   <Pallete pallete={generatePallete(seedColors[4])}/>
+      // </div>
+    );
+  }
 }
 
 export default App;
