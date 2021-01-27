7b838108ac834725e651a124c2a72902097921f2
sullie-dev <eoghan@sullie.dev>
2021-01-27 22:35:58 +0000

    Refactor CSS styling to js styling and removed CSS files

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,13 @@ import Snackbar from "@material-ui/core/Snackbar";
 import Close from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
 import Slider from "rc-slider";
-
 import "rc-slider/assets/index.css";
-import "./Navbar.css";
+// import "./Navbar.css";
+import styles from './styles/NavbarStyles'
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core";
 
-export default class Navbar extends Component {
+export default withStyles(styles)(class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = { format: "hex", open: false };
@@ -28,17 +29,17 @@ export default class Navbar extends Component {
   }
   render() {
     const { format } = this.state;
-    const { level, changeLevel, showColorBar } = this.props;
+    const { level, changeLevel, showColorBar, classes } = this.props;
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to={"/"}>React Color Picker</Link>
         </div>
         {showColorBar && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
 
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -50,7 +51,7 @@ export default class Navbar extends Component {
           </div>
         )}
 
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={this.state.format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -82,4 +83,4 @@ export default class Navbar extends Component {
       </header>
     );
   }
-}
+})
