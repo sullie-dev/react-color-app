bb7bd8bfc9167142de70f20d64186c28eee72896
sullie-dev <eoghan@sullie.dev>
2021-01-30 20:41:58 +0000

    Enhancment
    Added Drawer to new pallelete form

--- src/styles/PalleteListsStyles.js
+++ src/styles/PalleteListsStyles.js
@@ -1,46 +1,46 @@
 export default {
-    root: {
-      backgroundColor: "blue",
-      height: "100vh",
-      display: "flex",
-      alignItems: "flex-start",
-      justifyContent: "center",
-    },
-    container: {
-      width: "50%",
-      display: "flex",
-      alignItems: "flex-start",
-      flexDirection: "column",
-      flexWrap: "wrap",
-    },
-    nav: {
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between",
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      textDecoration: "none",
       color: "white",
-      alignItems:'center',
-      color:'white',
-      "& a":{
-        textDecoration:'none',
-        color:'white',
-        transition: '0.6s',
-      },
-      "& a :after":{
-        display: 'block',
-        content: "",
-        borderBottom: 'solid 3px white',
-        transform: 'scaleX(0)',
-        transition: "transform 250ms ease-in-out",
-      },
-      '& a :hover:after': {
-        transform: 'scaleX(.1)',
-      },
+      transition: "0.6s",
     },
-    palletes: {
-      boxSizing: "border-box",
-      width: "100%",
-      display: "grid",
-      gridTemplateColumns: "repeat(3,30%)",
-      gridGap: "5%",
+    "& a :after": {
+      display: "block",
+      content: "",
+      borderBottom: "solid 3px white",
+      transform: "scaleX(0)",
+      transition: "transform 250ms ease-in-out",
     },
-  };
+    "& a :hover:after": {
+      transform: "scaleX(.1)",
+    },
+  },
+  palletes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3,30%)",
+    gridGap: "5%",
+  },
+};
